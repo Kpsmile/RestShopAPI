6f7bdb37b2ec5fc50ee3662043189ce8fe13cd49
kamakhya.singh <kamakhya.singh@mcwaneindia.com>
2018-09-26 16:46:59 +0530

    admin changes

--- restshop/admin.py
+++ restshop/admin.py
@@ -5,6 +5,7 @@ from restshop.api.order_unit.models import OrderUnit
 from restshop.api.tag.models import Tag
 from restshop.api.unit.models import Unit, UnitImage
 from restshop.api.user.models import Seller
+from restshop.api.product.models import Product
 from .admin_models import UnitAdmin, ProductAdmin, UnitImageAdmin, OrderAdmin, OrderUnitAdmin
 
 admin.site.register([Seller,
@@ -15,3 +16,4 @@ admin.site.register(Unit, UnitAdmin)
 admin.site.register(UnitImage, UnitImageAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderUnit, OrderUnitAdmin)
+admin.site.register(Product, ProductAdmin)
